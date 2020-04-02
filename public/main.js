2251e997693d463fc18b32cb0a41e9cc45040c64
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-02 18:22:11 -0400

    working on testing the event listener click function when add button is clicked. removed the i element and replaced with plus sign on the add__btn class element

--- public/main.js
+++ public/main.js
@@ -1,23 +1,18 @@
+// Budget Controller
 var budgetController = (function() {
-  var x = 23
-
-  var add = function(a) {
-    return x + a
-  }
-
-  return {
-    publicTest: function(b) {
-      console.log(add(b))
-    }
-  }
+  // code here
 })()
 
+// UI Controller
 var UIController = (function() {
   // code here
 })()
 
 //Seperation of Concerns - each part of the application should be interested in doing one thing independently
 
+// Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
-  //code
-})(budgetController)
+  // document.querySelector('.add__btn').addEventListener('click', function() {
+  //   console.log('Button was clicked')
+  // })
+})(budgetController, UIController)
