adc16d12741b41817abadffc24aed0952c2ebf00
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-03 13:22:24 -0400

    working on the UI controller to return values of the add figure feature.

--- public/main.js
+++ public/main.js
@@ -5,14 +5,42 @@ var budgetController = (function() {
 
 // UI Controller
 var UIController = (function() {
-  // code here
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector('.add__type').value, // Will be either inc or exp
+        description: document.querySelector('.add__description').value,
+        value: document.querySelector('.add__value').value
+      }
+    }
+  }
 })()
 
 //Seperation of Concerns - each part of the application should be interested in doing one thing independently
 
 // Global App Controller
 var controller = (function(budgetCtrl, UICtrl) {
-  // document.querySelector('.add__btn').addEventListener('click', function() {
-  //   console.log('Button was clicked')
-  // })
+  var ctrlAddItem = function() {
+    // 1. Get the field input data
+
+    var input = UICtrl.getInput()
+    console.log(input)
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+    console.log('enter was pressed')
+  }
+
+  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+  document.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem()
+    }
+  })
 })(budgetController, UIController)
