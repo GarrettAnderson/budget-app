cb14ad1cf85fd78b84705e652e8c7d9de4a75d68
GarrettAnderson <garrettleegrahamanderson@gmail.com>
2020-04-21 17:14:25 -0400

    fixed the bug where expense wasnt being added to the UI

--- public/main.js
+++ public/main.js
@@ -62,14 +62,14 @@ var UIController = (function() {
     inputValue: '.add__value',
     inputBtn: '.add__btn',
     incomeContainer: '.income__list',
-    expensesContainer: '.expense__list'
+    expensesContainer: '.expenses__list'
   }
   return {
     getInput: function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       }
     },
 
@@ -85,7 +85,7 @@ var UIController = (function() {
       } else if (type === 'exp') {
         element = DOMstrings.expensesContainer
         html =
-          ' <div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button> </div> </div> </div>'
+          ' <div class="item clearfix" id="expense-%id%"> <div class="item__description">%description%</div> <div class="right clearfix"> <div class="item__value">%value%</div> <div class="item__percentage">21%</div> <div class="item__delete"> <button class="item__delete--btn">X</button> </div> </div> </div>'
       }
 
       // Replace the placeholder text with some actual data
@@ -96,6 +96,19 @@ var UIController = (function() {
 
       // Insert the HTML into the DOM
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml)
+    },
+
+    clearFields: function() {
+      var fields, fieldsArr
+
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue)
+
+      fieldsArr = Array.prototype.slice.call(fields)
+
+      fieldsArr.forEach(function(current, index, array) {
+        current.value = ''
+      })
+      fieldsArr[0].focus()
     },
 
     getDOMstrings: function() {
@@ -120,24 +133,31 @@ var controller = (function(budgetCtrl, UICtrl) {
 
   var DOM = UICtrl.getDOMstrings()
 
+  var updateBudget = function() {
+    // 1. Calculate the budget
+    // 2. Return the budget
+    // 3. Display the budget on the UI
+  }
+
   var ctrlAddItem = function() {
     var input, newItem
 
     // 1. Get the field input data
-
     input = UICtrl.getInput()
     console.log(input)
 
-    // 2. Add the item to the budget controller
+    if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
+      // 2. Add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value)
+      // 3. Add the item to the UI
+      UICtrl.addListItem(newItem, input.type)
 
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value)
-    // 3. Add the item to the UI
-    UICtrl.addListItem(newItem, input.type)
+      // 4. Clear the fields
+      UICtrl.clearFields()
 
-    // 4. Calculate the budget
-
-    // 5. Display the budget on the UI
-    console.log('enter was pressed')
+      // 5. Calculate and update budget
+      updateBudget()
+    }
   }
 
   return {
